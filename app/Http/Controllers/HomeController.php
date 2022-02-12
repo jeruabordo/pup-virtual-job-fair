b72a9ef5e6bd54b113bafd017e9dcf8beda7d785
jeruabordo <gellyneabordo@gmail.com>
2022-02-12 15:58:48 +0800

    rename view dashboards/student to dashboard/profile

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,6 +21,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboards.student');
+        return view('dashboard.profile');
     }
 }
